2dece8cf7b2cbc0192a29561f8abb1122b29c105
Pedro Miguel do Vale Moreira <2411672+pedromoreira-estg@users.noreply.github.com>
2025-05-30 10:13:07 +0100

    front+back

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = new express();
+
+app.use(cors());
+
+const PORT = 3000;
+
+app.get("/nomes", async (req, res) => {
+    res.json([{"nome":"eva"},{"nome":"adão"},{"nome":"abel"}])
+})
+
+app.listen(PORT, () => {
+    console.log("servidor ligado na porta "+PORT)
+})
---
+++ frontend/script.js
@@ -0,0 +1,17 @@
+const APIURL = "http://localhost:3000/nomes"
+async function mostranomes() {
+    // identificar o div para colocar os nomes
+    const divnomes = document.getElementById("lista");
+    // reset do conteúdo
+    divnomes.innerHTML = "";
+    respostaapi = await fetch(APIURL);
+    nomesapi = await respostaapi.json();
+
+    for (cadanome of nomesapi) {
+        let novop = document.createElement("p");
+        novop.innerHTML = cadanome.nome;
+        divnomes.appendChild(novop);
+    } 
+}
+
+mostranomes();
